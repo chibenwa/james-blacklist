85d56524d735ded998a4a3a1778614a6e3934447
Benoit Tellier <btellier@linagora.com>
2019-10-17 12:29:40 +0700

    ACEU step 5

--- blacklist-webadmin/src/main/java/com/linagora/james/blacklist/webadmin/BlackListRoutes.java
+++ blacklist-webadmin/src/main/java/com/linagora/james/blacklist/webadmin/BlackListRoutes.java
@@ -19,12 +19,30 @@
 
 package com.linagora.james.blacklist.webadmin;
 
+import javax.inject.Inject;
+
+import org.apache.james.core.Domain;
+import org.apache.james.core.MailAddress;
 import org.apache.james.webadmin.Routes;
+import org.apache.james.webadmin.utils.JsonTransformer;
+import org.apache.james.webadmin.utils.Responses;
+
+import com.github.steveash.guavate.Guavate;
+import com.linagora.james.blacklist.api.PerDomainAddressBlackList;
 
 import spark.Service;
 
 public class BlackListRoutes implements Routes {
     private static final String BASE_PATH = "/backList";
+
+    private final PerDomainAddressBlackList blackList;
+    private final JsonTransformer jsonTransformer;
+
+    @Inject
+    public BlackListRoutes(PerDomainAddressBlackList blackList) {
+        this.blackList = blackList;
+        this.jsonTransformer = new JsonTransformer();
+    }
 
     @Override
     public String getBasePath() {
@@ -33,6 +51,32 @@ public class BlackListRoutes implements Routes {
 
     @Override
     public void define(Service service) {
-        // todo ACEU 19 step 4
+        service.get(getBasePath() + "/:domain", (req, res) -> {
+            Domain domain = Domain.of(req.params("domain"));
+            return blackList.list(domain)
+                .stream()
+                .map(MailAddress::asString)
+                .collect(Guavate.toImmutableList());
+        }, jsonTransformer);
+
+        service.delete(getBasePath() + "/:domain", (req, res) -> {
+            Domain domain = Domain.of(req.params("domain"));
+            blackList.clear(domain);
+            return Responses.returnNoContent(res);
+        }, jsonTransformer);
+
+        service.put(getBasePath() + "/:domain/:maddress", (req, res) -> {
+            Domain domain = Domain.of(req.params("domain"));
+            MailAddress address = new MailAddress(req.params("maddress"));
+            blackList.add(domain, address);
+            return Responses.returnNoContent(res);
+        }, jsonTransformer);
+
+        service.delete(getBasePath() + "/:domain/:maddress", (req, res) -> {
+            Domain domain = Domain.of(req.params("domain"));
+            MailAddress address = new MailAddress(req.params("maddress"));
+            blackList.remove(domain, address);
+            return Responses.returnNoContent(res);
+        }, jsonTransformer);
     }
 }
